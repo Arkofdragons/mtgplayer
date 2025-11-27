e6d059c1bcc0c5e61496e2b336bd483cb0029bf9
Arkofdragons <arkofdragons@zohomail.com>
2025-11-27 00:21:06 -0700

    Create application

---
+++ composeApp/src/commonTest/kotlin/com/arkofdragons/mtgplayer/ComposeAppCommonTest.kt
@@ -0,0 +1,12 @@
+package com.arkofdragons.mtgplayer
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
---
+++ composeApp/src/jvmMain/kotlin/com/arkofdragons/mtgplayer/Platform.jvm.kt
@@ -0,0 +1,7 @@
+package com.arkofdragons.mtgplayer
+
+class JVMPlatform: Platform {
+    override val name: String = "Java ${System.getProperty("java.version")}"
+}
+
+actual fun getPlatform(): Platform = JVMPlatform()
